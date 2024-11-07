9e4cf2a618000dc3d672af9c6e558724534e5261
ellexart <axelle.mazuy@gmail.com>
2024-11-07 20:14:25 +0100

    feat: SpeedTypingGame entity

--- PatteDoie/Models/SpeedTyping/SpeedTypingGame.cs
+++ PatteDoie/Models/SpeedTyping/SpeedTypingGame.cs
@@ -2,5 +2,9 @@
 {
     public class SpeedTypingGame
     {
+        public Guid Id { get; set; }
+        public SpeedTypingScore[] Scores { get; set; }
+        public SpeedTypingTimeProgress[] TimeProgresses { get; set; }
+        public DateTime LaunchTime { get; set; }
     }
 }
