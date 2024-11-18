990beeb3068f1f7d8a4d0489378948465785c1e1
ellexart <axelle.mazuy@gmail.com>
2024-11-18 22:39:01 +0100

    feat: change cshtml GetGame to razor component, add input field

---
+++ PatteDoie/Views/SpeedTypingGames/GetGame.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using PatteDoie.Models.SpeedTyping;
+using PatteDoie.Rows.SpeedTypingGame;
+using PatteDoie.Services.SpeedTyping;
+
+namespace PatteDoie.Views.SpeedTypingGames
+{
+    public partial class GetGame : ComponentBase
+    {
+        public SpeedTypingGameRow GameRow;
+
+        public SpeedTypingService Service;
+
+        public SpeedTypingGame Model;
+
+        public void CheckTextSpace(string Text)
+        {
+            if (Text.Contains(' '))
+            {
+                Console.WriteLine("success");
+            }
+        }
+    }
+}
