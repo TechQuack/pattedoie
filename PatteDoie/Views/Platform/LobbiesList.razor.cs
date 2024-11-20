61e044859863697d8b90df9eecd24c39985d7d0b
Etienne Binginot <etienne.binginot@gmail.com>
2024-11-20 21:01:33 +0100

    feat: update lobbies list in blazor code

---
+++ PatteDoie/Views/Platform/LobbiesList.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+using PatteDoie.Models.Platform;
+
+namespace PatteDoie.Views.Platform;
+
+public partial class LobbiesList : ComponentBase
+{
+    public PlatformLobby[] Items = [];
+}
