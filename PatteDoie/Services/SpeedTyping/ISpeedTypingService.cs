258ede832f89cb5fbd89097f942a1b58c2973717
vanliflo <florian.van-liedekerke@univ-rouen.fr>
2024-11-20 21:55:54 +0100

    feat: add method to check if the input is correct

--- PatteDoie/Services/SpeedTyping/ISpeedTypingService.cs
+++ PatteDoie/Services/SpeedTyping/ISpeedTypingService.cs
@@ -17,5 +17,7 @@ namespace PatteDoie.Services.SpeedTyping
         public Task<SpeedTypingGameRow> CreateGame(CreateSpeedTypingGameCommand command, PlatformUser[] platformUsers);
 
         public Task DeleteGame(Guid id);
+
+        public Task<bool> CheckWord(Guid gameId, Guid playerId, string word);
     }
 }
