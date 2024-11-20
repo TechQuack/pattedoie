fdd25d262af881265c3ba0f22f25693786772b81
Etienne Binginot <etienne.binginot@gmail.com>
2024-11-20 19:34:15 +0100

    feat: add user authentication with creation of token

---
+++ PatteDoie/Views/Home/Authenticator.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace PatteDoie.Views.Home;
+
+public partial class Authenticator : ComponentBase
+{
+    [Inject]
+    private ProtectedLocalStorage ProtectedLocalStorage { get; set; } = default!;
+
+    private string Name { get; set; } = string.Empty;
+
+
+    private async Task RegisterUser()
+    {
+        await ProtectedLocalStorage.SetAsync("name", Name);
+        await GenerateUUID();
+    }
+
+    private async Task GenerateUUID()
+    {
+        var uuid = Guid.NewGuid().ToString();
+        await ProtectedLocalStorage.SetAsync("uuid", uuid);
+    }
+}
