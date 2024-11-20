908a8d5d4958d411fd214539e0ac68313bfdc6d6
vanliflo <florian.van-liedekerke@univ-rouen.fr>
2024-11-20 21:55:02 +0100

    feat: change array attributes to ICollection

--- PatteDoie/Models/SpeedTyping/SpeedTypingGame.cs
+++ PatteDoie/Models/SpeedTyping/SpeedTypingGame.cs
@@ -3,8 +3,8 @@
     public class SpeedTypingGame
     {
         public Guid Id { get; set; }
-        public SpeedTypingScore[] Scores { get; set; }
-        public SpeedTypingTimeProgress[] TimeProgresses { get; set; }
+        public ICollection<SpeedTypingScore> Scores { get; set; }
+        public ICollection<SpeedTypingTimeProgress> TimeProgresses { get; set; }
         public DateTime LaunchTime { get; set; }
         public string[] Words { get; set; }
     }
--- PatteDoie/Rows/SpeedTyping/SpeedTypingGameRow.cs
+++ PatteDoie/Rows/SpeedTyping/SpeedTypingGameRow.cs
@@ -5,8 +5,8 @@ namespace PatteDoie.Rows.SpeedTypingGame
     public class SpeedTypingGameRow
     {
         public Guid Id { get; set; }
-        public SpeedTypingScore[] Scores { get; set; }
-        public SpeedTypingTimeProgress[] TimeProgresses { get; set; }
+        public ICollection<SpeedTypingScore> Scores { get; set; }
+        public ICollection<SpeedTypingTimeProgress> TimeProgresses { get; set; }
         public DateTime LaunchTime { get; set; }
         public string[] Words { get; set; }
     }
