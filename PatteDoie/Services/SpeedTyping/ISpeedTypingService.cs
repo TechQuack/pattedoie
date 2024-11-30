1e127d4eb1335485903d183b7e3f50f4a00ba31a
vanliflo <florian.van-liedekerke@univ-rouen.fr>
2024-11-30 09:10:38 +0100

    fix: word to write is now correctly displayed even after refreshing

--- PatteDoie/Services/SpeedTyping/ISpeedTypingService.cs
+++ PatteDoie/Services/SpeedTyping/ISpeedTypingService.cs
@@ -16,11 +16,12 @@ namespace PatteDoie.Services.SpeedTyping
         public Task SetTimeProgress(SpeedTypingGame game, SpeedTypingPlayer player, DateTime timeProgress);
 
         public Task<SpeedTypingGameRow> CreateGame(List<User> platformUsers);
-            
+
         public Task DeleteGame(Guid id);
 
         public Task<bool> CheckWord(Guid gameId, Guid playerId, string word);
 
+        public Task<int> GetScore(Guid playerId);
         public Task ManageEndOfGame(Guid gameId);
     }
 }
