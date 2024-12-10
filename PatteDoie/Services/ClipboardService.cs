1d0f83d844e758a794aa94f4e472a534e43841e2
vanliflo <florian.van-liedekerke@univ-rouen.fr>
2024-12-10 21:28:13 +0100

    feat: add button to copy lobby link

---
+++ PatteDoie/Services/ClipboardService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.JSInterop;
+
+namespace PatteDoie.Services
+{
+    public class ClipboardService(IJSRuntime JsRuntTime) : IClipboardService
+    {
+        private readonly IJSRuntime JsRuntTime = JsRuntTime;
+
+        public async Task CopyLobbyLink(Guid lobbyId)
+        {
+            await JsRuntTime.InvokeAsync<string>("navigator.clipboard.writeText", "https://localhost:8081/lobby/join/" + lobbyId);
+        }
+    }
+}
